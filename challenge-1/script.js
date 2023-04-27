8bb66c02c8aac88215605f657dedc34db4a3d764
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-04-27 09:59:23 +0200

    Finalized code modification

--- challenge-1/script.js
+++ challenge-1/script.js
@@ -1,20 +1,26 @@
 // define constant global variables
 
-const FREE_WARNING = 'Free shipping only applies to single customer orders'
-const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
-const NONE_SELECTED = 0 /* value should be numerical---cannot multiply later with a string */
+const FREE_WARNING = 'Free shipping only applies to single customer orders';
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
+const NONE_SELECTED = 0; /* value should be numerical---cannot multiply later with a string */
 
 // moved code closer to beginning of code and declared variables
-const customers = 1 /* customers should be recorded in numericals not stored as a string */
-let location = 'RSA'
-let currency = null
+const customers = 1; /* customers should be recorded in numericals not stored as a string */
+let location = 'RSA';
+let currency = null;
+let shipping = null;
 
 // improve readability and modify math operators for functionality
+// calcShipping was an undefined variable ∴ could not be fetched---made use of it here
 
 if (location === 'RSA') { 
-    shipping = 400;
-    currency = 'R';
+    const calcShipping = 400;    /* couldn't reassign new values to shipping and currency outside the block-scope */
+    const newCurrency = 'R';    /* renamed variables to avoid block-scope syntax error */
+    shipping = calcShipping;     /* used new variables as new values of the original variables given */
+    currency = newCurrency;     /* used camelCase */
 }
+
+console.log('Shipping:', currency, shipping);
 
 if (location === 'NAM') {
     shipping = 600;
@@ -32,16 +38,33 @@ const shirts = 150 * NONE_SELECTED;  /* NONE_SELECTED variable should not be a s
 const batteries = 35 * 2;
 const pens = 5 * NONE_SELECTED; 
 
-const shipping = null;
-let currency = '$';
+/* Improved functionality of code
+ * Each location has its own if statement to check for total cost without shipping using && operator
+ * Combined the two if statements for each location into one if statement using || operator
+ * Code could also be written as follows and have full functionality:
+ * 
+ *  if (location === 'RSA' && customers === 1) {
+ *      if (shoes + batteries + pens + shirts >= 1000) {
+ *          shipping = 0;
+ *      }
+ *  }
+ *
+ *  if (location === 'NAM' && customers === 1) {
+ *      if (shoes + batteries + pens + shirts >= 60) {
+ *          shipping = 0;
+ *      }
+ *  }
+ *
+ */
 
-if (shoes + batteries + pens + shirts > 1000 ) {
-	if (location === 'NAM' && customers < 2) {
-			if (location === 'RSA'){
-		    shipping = 0 || calcShipping
-		}
-	}
+
+// Wrapped code for better readability
+
+if ((location === 'RSA' && customers === 1 && shoes + batteries + pens + shirts >= 1000) ||
+    (location === 'NAM' && customers === 1 && shoes + batteries + pens + shirts >= 60)) {
+    shipping = 0;
 }
+
 
 // WARNING is not a valid variable---fixed strict equality checker in code
 
@@ -50,6 +73,7 @@ if (shipping === 0 && customers !== 1) {
 }
 
 // WARNING variable in console.log cannot be fetched because it was never declared in the code
+// Added strict equality checker in ternary
 
-let location = ('NK' ? console.log(BANNED_WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping))
+location === ('NK' ? console.log(BANNED_WARNING) : console.log('Price:', currency, shoes + batteries + pens + shirts + shipping));
 
